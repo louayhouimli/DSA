7e99f8304f13d7d81893e49c15372c81619c50d5
Louay Houimli <141954755+louayhouimli@users.noreply.github.com>
2026-08-20 08:53:42 +0100

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        return countS == countT
+
+        # time complexity is O(n+m) where n is the length of s and m is the length of t
+        # space complexity is O(1) because we have only 26 letters in english
